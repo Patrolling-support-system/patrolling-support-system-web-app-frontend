48b9b3c08a51ce9eaa9f49ee0177cd1c41b98175
Majkel <msterzel@student.agh.edu.pl>
2023-05-25 18:24:37 +0200

    Fixed bug preventing chat component from rendering

--- src/TaskDetails.js
+++ src/TaskDetails.js
@@ -163,7 +163,7 @@ export function TaskDetails() {
         <CheckpointsView documentData={documentData} setSignal={setSignal} />
       );
     } else if (page === "chat") {
-      return <div>Madzia</div>;
+      return <PatrolGroupChatComponent documentData={documentData}/>;
     }
   };
 
@@ -191,10 +191,11 @@ export function TaskDetails() {
         </ListItemIcon>
         <ListItemText primary="Checkpoints" />
       </ListItemButton>
-      <ListItemButton
+      <ListItemButton onClick={() => setCurrentPage("chat")}>
+        {/* <ListItemButton
         onClick={() =>
           handleItemClick(<PatrolGroupChatComponent documentData={documentData} />)
-        }>
+        }> */}
         <ListItemIcon>
           <ChatIcon />
         </ListItemIcon>
@@ -218,88 +219,88 @@ export function TaskDetails() {
 
   return (
     <ThemeProvider theme={mdTheme}>
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar position="absolute" open={open}>
-        <Toolbar
-          sx={{
-            pr: "24px", // keep right padding when drawer closed
-          }}
-        >
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={toggleDrawer}
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
             sx={{
-              marginRight: "36px",
-              ...(open && { display: "none" }),
+              pr: "24px", // keep right padding when drawer closed
             }}
           >
-            <MenuIcon />
-          </IconButton>
-          {isLoaded ? (
-            <Typography
-              component="h1"
-              variant="h6"
+            <IconButton
+              edge="start"
               color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
+              }}
             >
-              Task details
-            </Typography>
-          ) : (
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Loading data...
-            </Typography>
-          )}
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <Toolbar
+              <MenuIcon />
+            </IconButton>
+            {isLoaded ? (
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                Task details
+              </Typography>
+            ) : (
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                Loading data...
+              </Typography>
+            )}
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {mainListItems}
+            <Divider sx={{ my: 1 }} />
+            {secondaryListItems}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
           sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            px: [1],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
           }}
         >
-          <IconButton onClick={toggleDrawer}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </Toolbar>
-        <Divider />
-        <List component="nav">
-          {mainListItems}
-          <Divider sx={{ my: 1 }} />
-          {secondaryListItems}
-        </List>
-      </Drawer>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          height: "100vh",
-          overflow: "auto",
-        }}
-      >
-        <Toolbar />
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-          {isLoaded ? (
-            <div className="render-container">{selectedComponent}</div>
-          ) : (
-            <Typography>Loading...</Typography>
-          )}
-        </Container>
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            {isLoaded ? (
+              <div className="render-container">{selectedComponent}</div>
+            ) : (
+              <Typography>Loading...</Typography>
+            )}
+          </Container>
+        </Box>
       </Box>
-    </Box>
-  </ThemeProvider>
+    </ThemeProvider>
     // <ThemeProvider theme={mdTheme}>
     //   <Box sx={{ display: 'flex' }}>
     //     <CssBaseline />
